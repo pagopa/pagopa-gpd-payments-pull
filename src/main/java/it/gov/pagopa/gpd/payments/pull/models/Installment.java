90277e8d84e98461195369f879850b47cf4fd61f
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-03-25 14:54:31 +0100

    [VAS-869] feat: Updated openapi.json

--- src/main/java/it/gov/pagopa/gpd/payments/pull/models/Installment.java
+++ src/main/java/it/gov/pagopa/gpd/payments/pull/models/Installment.java
@@ -6,8 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -20,18 +20,33 @@ import java.time.LocalDate;
 @RegisterForReflection
 public class Installment implements Serializable {
 
+    @Schema(description = "Notice Code", required = true)
     private String nav;
+    @Schema(description = "Unique identifier", required = true)
     private String iuv;
+    @Schema(description = "Tax code of the Creditor Body", required = true)
     private String paTaxCode;
+    @Schema(description = "Company name of the Creditor Body", required = true)
     private String paFullName;
+    @Schema(description = "Payment Option Amount", required = true)
     private BigDecimal amount;
+    @Schema(description = "Description of the OP (e.g. \"SISA - 741T, 942T - Checks without authorization or funding\")",
+            required = true)
     private String description;
+    @Schema(description = "Indicates whether the OP is part of an installment plan")
     private Boolean isPartialPayment;
+    @Schema(description = "Is the date by which (TO) the Payment option is payable.", required = true)
     private LocalDate dueDate;
+    @Schema(description = "Not currently used in any logic. The purpose of this date will be to give the possibility" +
+            " to specify a time period after the dueDate within which a payment option, even if expired, will still be payable.")
     private LocalDate retentionDate;
+    @Schema(description = "date of insertion of the OP", required = true)
     private LocalDate insertedDate;
+    @Schema(description = "corresponds to the SEND notification costs")
     private BigDecimal notificationFee;
+    @Schema(description = "Status of the OP", required = true)
     private String status;
+    @Schema(description = "OP update date", required = true)
     private LocalDate lastUpdatedDate;
 
 }
--- src/main/java/it/gov/pagopa/gpd/payments/pull/models/PaymentOption.java
+++ src/main/java/it/gov/pagopa/gpd/payments/pull/models/PaymentOption.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -19,14 +20,24 @@ import java.util.List;
 @RegisterForReflection
 public class PaymentOption implements Serializable {
 
+    @Schema(description = "Description of the payment option")
     private String description;
 
+    @Schema(description = "Number of installments that make up the payment option, " +
+            "if equal to 1 it is a single payment",
+            required = true)
     private Integer numberOfInstallments;
 
+    @Schema(description = "Is the date by which the Payment option is payable.")
     private LocalDate dueDate;
 
+    @Schema(description = "Indicates, if set to true, in the case of PD created on GPD," +
+            " that once the expiration date (dueDate ) has passed the PD is automatically set to the expired status",
+            required = true )
     private Boolean switchToExpired;
 
+    @Schema(description = "Array structure containing the installments that make up " +
+            "the payment option (there always exists at least 1)", required = true)
     private List<Installment> installments;
 
 }
