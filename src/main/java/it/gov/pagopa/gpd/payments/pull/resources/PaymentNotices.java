9a33ce0286f41b39381cda828caffc38baac6613
Jacopo <jacopo.carlini@gmail.com>
2024-03-27 16:52:09 +0100

    [VAS-729] jacoco path

--- src/main/java/it/gov/pagopa/gpd/payments/pull/resources/PaymentNotices.java
+++ src/main/java/it/gov/pagopa/gpd/payments/pull/resources/PaymentNotices.java
@@ -50,16 +50,17 @@ public class PaymentNotices {
     /**
      * Recovers a reactive stream of payment notices, using the debtor taxCode, and optionally the dueDate for which at least one
      * Payment Option must be valid. Uses limit and page to limit result size
+     *
      * @param taxCode debtor tax code to use for notices search. mandatory
      * @param dueDate optional parameter to filter notices based on valid dueDate
-     * @param limit page limit
-     * @param page page number
+     * @param limit   page limit
+     * @param page    page number
      * @return Response containing a reactive stream containing a list of notices, or a ErrorResponse in case of KO
      */
     @Operation(
             summary = "Get Payment Notices",
             description = "Retrieve payment notices from ACA and GPD sources, filtered by a due date")
-    @SecurityRequirement(name="ApiKey")
+    @SecurityRequirement(name = "ApiKey")
     @APIResponses(value = {
             @APIResponse(responseCode = "200", description = "OK",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON,
@@ -73,30 +74,29 @@ public class PaymentNotices {
     @GET
     public Uni<Response> getPaymentNotices(
             @Parameter(description = "Tax code to use for retrieving notices", required = true)
-                @HeaderParam("x-tax-code") String taxCode,
+            @HeaderParam("x-tax-code") String taxCode,
             @Parameter(description = "Optional date to filter paymentNotices (if provided use the format yyyy-MM-dd)")
-                @QueryParam("dueDate") LocalDate dueDate,
+            @QueryParam("dueDate") LocalDate dueDate,
             @Valid @Positive @Max(100) @Parameter(description = "Number of elements on one page. Default = 50")
-                @DefaultValue("50") @QueryParam("limit") Integer limit,
+            @DefaultValue("50") @QueryParam("limit") Integer limit,
             @Valid @Min(0) @Parameter(description = "Page number. Page value starts from 0")
             @DefaultValue("0") @QueryParam("page") Integer page
     ) {
 
-        if (taxCode == null || taxCode.length() != FISCAL_CODE_LENGTH) {
+        if(taxCode == null || taxCode.length() != FISCAL_CODE_LENGTH) {
             String errMsg = "Fiscal code header is null or not valid";
             throw new InvalidTaxCodeHeaderException(AppErrorCodeEnum.PPL_601, errMsg);
         }
         // replace new line and tab from user input to avoid log injection
         taxCode = taxCode.replaceAll(REGEX, REPLACEMENT);
 
-        Uni<List<PaymentNotice>> paymentNoticesUni = paymentNoticeService
-                .getPaymentNotices(taxCode, dueDate, limit, page);
+        Uni<List<PaymentNotice>> paymentNoticesUni = paymentNoticeService.getPaymentNotices(taxCode, dueDate, limit, page);
         return paymentNoticesUni.onFailure().invoke(error -> {
-                    if (error instanceof PaymentNoticeException)
-                        throw new PaymentNoticeException(((PaymentNoticeException) error)
-                                .getErrorCode(),error.getMessage(),error.getCause());
-                    else
-                        throw new RuntimeException(error);
+            if(error instanceof PaymentNoticeException)
+                throw new PaymentNoticeException(((PaymentNoticeException) error)
+                        .getErrorCode(), error.getMessage(), error.getCause());
+            else
+                throw new RuntimeException(error);
         }).onItem().transform(item -> Response.ok().entity(item).build());
     }
 
