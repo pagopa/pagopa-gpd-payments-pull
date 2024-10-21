1ea4d3a1ba7084707bcbba241d1016630ade4711
giomella <giomella@emeal.nttdata.com>
2024-10-21 12:11:38 +0200

    remove unused code

--- src/main/java/it/gov/pagopa/gpd/payments/pull/exception/AppErrorException.java
+++ src/main/java/it/gov/pagopa/gpd/payments/pull/exception/AppErrorException.java
@@ -2,25 +2,13 @@ package it.gov.pagopa.gpd.payments.pull.exception;
 
 import lombok.Getter;
 
-import java.time.LocalDate;
-
 /**
  * Base exception for PDF Engine exceptions
  */
 @Getter
 public class AppErrorException extends RuntimeException {
 
-    private String taxCode;
-    private LocalDate dueDate;
-
-
     public AppErrorException(Throwable error) {
         super(error);
-    }
-
-    public AppErrorException(Throwable cause, LocalDate dueDate, String taxCode) {
-        super(cause);
-        this.dueDate = dueDate;
-        this.taxCode = taxCode;
     }
 }
